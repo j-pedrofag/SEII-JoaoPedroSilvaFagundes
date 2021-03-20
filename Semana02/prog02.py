8faec7176b6ac6078f2d174bb30599f6e5523377
j-pedrofag <80978997+j-pedrofag@users.noreply.github.com>
2021-03-19 22:45:00 -0300

    Código 02

---
+++ Semana02/prog02.py
@@ -0,0 +1,74 @@
+
+#Descobrindo o tipo do numeral 
+num = 3.14
+print(type(num))
+
+# Arithmetic Operators:
+# Adição:      
+print(3 + 2)
+
+# Subtração:   
+print(3 - 2)
+
+# Multiplicação: 
+print(3 * 2)
+
+# Divisão:
+print(3 / 2)
+
+# Floor divisão:
+print(3 // 2)
+
+# Exponencial:
+print(3 ** 2)
+
+# Resto da divisão:
+print(3 % 2)
+
+# Implementações:
+num =1 
+num = num + 1
+print(num)
+
+#Ou:
+num +=1
+print(num)
+
+# Comando de módulo:
+print(abs(-3))
+
+# Comando de arredondamento:
+print(round(3.75)) 
+
+#Arredondando para 1 casa:
+print(round(3.75,1))
+
+# Comparações:
+# Igualdade:   
+num_1 = 3
+num_2 = 2
+print(num_1 == num_2)
+
+# Desigualdade: 
+print(num_1!=num_2)
+
+# Maior que:
+print(num_1 > num_2)
+
+# Menor que:
+print(num_1 < num_2)
+
+# Maior ou igual:
+print(num_1 >= num_2)
+
+# Menor ou igual: 
+print(num_1 <= num_2)
+
+# Trabsformando strins em numerais 
+num_1 = '100'
+num_2 = '200'
+
+num_1 = int('100')
+num_2 = int('200')
+
+print(num_1 + num_2)
