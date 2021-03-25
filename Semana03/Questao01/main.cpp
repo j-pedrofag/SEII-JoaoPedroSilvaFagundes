29305266a0656bec94c9e6e9cbe618630ba47959
j-pedrofag <80978997+j-pedrofag@users.noreply.github.com>
2021-03-24 22:55:16 -0300

    Atualização arquivos

---
+++ Semana03/Questao01/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "ImagNum.h"
+
+using namespace std;
+using namespace ImagComp;
+
+int main(){
+    ImagNum x(1,1),y(2,2);
+    ImagNum* z = x*y;
+    cout<< z->GetValueCart().a << " " << z->GetValueCart().b << endl;
+    return 0;
+}
